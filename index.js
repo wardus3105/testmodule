3ec3552d4a0e1a13626e4fd9c7bbf880bc6eccdc
Wardus <mobile-backend>
2021-02-25 10:09:40 +0700

    5t

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fetch = require('node-fetch');  
-
-exports.testFetch = fetch('https://api.github.com/users/github')
+exports.testFetch = function() {
+    fetch('https://api.github.com/users/github')
         .then(res => res.json())
         .then(json => console.log(json));
+}
+
